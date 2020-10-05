504a5acb1cfe5df7575584cb93c92b977a316351
gaum123 <gorsel@roc-dev.nl>
2020-10-05 22:26:57 +0200

    KlasOpdracht ScrumTeam

---
+++ src/CooleOpdracht.java
@@ -0,0 +1,20 @@
+import java.applet.*;
+import java.awt.*;
+
+public class CooleOpdracht extends Applet {
+    int y = 20;
+    int opsomming = 0;
+    int getal = 13;
+    // = is word
+    public void init() {
+        System.out.println("Hallo");
+        for (int i = 0; i <= getal; i++) {
+            int temp = i;
+            if (getal%2 == 0)
+        }
+    }
+
+    public void paint(Graphics g) {
+
+    }
+}
